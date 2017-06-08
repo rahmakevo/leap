23a43b49b968a98ce4ee4c8d9414796007e2adff
rahmakevo <siundu344@gmail.com>
2017-06-08 13:55:10 +0300

    add leap-year project

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+//user interface logic
+$(document).ready(function(){
+  $("form#leap-year").submit(function(event){
+    event.preventDefault();
+
+    //business logic
+    var leapYear = function(year){
+      if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+        return true;
+      } else {
+        return false;
+      }
+    };
+    
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+});
+})
